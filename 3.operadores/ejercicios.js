ec4e834e6d705d284557ddc70e3985fe46279266
Junior <juwloquispe18@gmail.com>
2022-10-20 10:00:40 -0500

    Avance de 10:00 am

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,28 @@
+// EJEMPLOS 
+//OPERADOR DE ASIGNACION
+// (+=) X+=2  Es equivalente a: x=x+2= 7
+// (-=) x-=2  Es equivalente a: x=x-2=5
+// (*=) x*=2  Es equivalente a: x=x*2=14
+// (/=) x/=2  Es equivalente a: x=x/2=3.5
+
+// EJEMPLOS :
+// OPERADORES DE COMPARACIÓN :
+//	3 == var1 
+// "3" == var1 
+// 3 == '3'
+
+//var1 
+// != 4 var2 
+// !== "3"
+
+// 3 === var1
+
+// var1 !== "3" //3
+//!== '3'
+
+//var2 > var1 "12" > 2
+
+//var2 >= var1 var1 >= 3
+
+// var1 < var2 "2" < 12
+// var1 <= var2 var2 <= 5
