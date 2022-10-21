fb106f52f9f1bcafb079f10954ac0478d40298ea
Junior <juwloquispe18@gmail.com>
2022-10-21 18:08:46 -0500

    Trabajo estructura secuencial javascript

---
+++ 4.estructuraSecuencial/ejercicios.js
@@ -0,0 +1,25 @@
+//EJERCICIOS :
+//ESTRUCTURA SECUENCIAL
+// Ejemplo de un algoritmo con estructura secuencial:
+// Realiza la carga de dos números por teclado e imprimir su suma y su producto:
+
+//<html>
+{/* <head>
+<script type="text/javascript">
+  var valor1;
+  var valor2;
+  valor1=prompt('Ingrese primer número:','');
+  valor2=prompt('Ingrese segundo número','');
+  var suma=parseInt(valor1)+parseInt(valor2);
+  var producto=parseInt(valor1)*parseInt(valor2);
+  document.write('La suma es ');
+  document.write(suma);
+  document.write('<br>');
+  document.write('El producto es ');
+  document.write(producto);
+</script>
+</head>
+<body>
+</body>
+</html>
+ */}
---
+++ 4.estructuraSecuencial/index.js
@@ -0,0 +1,5 @@
+//SIGNIFICADO DE ESTRUCTURA SECUENCIAL
+//ESTRUCTURA SECUENCIAL
+//Una estructura secuencial es aquella en la que una acción (instrucción) sigue a otra en secuencia. 
+//Las tareas se suceden de tal modo que la salida de una es la entrada de la siguiente y así sucesivamente hasta el final del proceso.
+// En este tipo de programación se presentan operaciones de inicio a fin, inicialización de variables, operaciones de asignación, cálculo, sumarización, entre otras.
